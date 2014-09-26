01843dbd9855c97831b17952b449ff4836adfe03
nahida1234 <nahida_036@yahoo.com>
2014-09-26 09:47:38 +0600

    queue management application

---
+++ QueueManagementApp.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QueueManagementApplication
+{
+    public partial class QueueManagementApp : Form
+    {
+        public QueueManagementApp()
+        {
+            InitializeComponent();
+        }
+
+
+        Queue<CustomerInfo> customerInfos=new Queue<CustomerInfo>();
+        public int serialNumber = 0;
+        private void enqueueButton_Click(object sender, EventArgs e)
+
+        {
+            CustomerInfo customer = new CustomerInfo();
+            customer.serialNo = Convert.ToString(++serialNumber);
+            customer.name = enqueueNameTextBox.Text;
+            customer.complain = enqueueComplainTextBox.Text;
+
+            customerInfos.Enqueue(customer);
+
+            MessageBox.Show(customer.name +" your serial no is" +customer.serialNo);
+
+            ListViewItem cuListViewItem=new ListViewItem(customer.serialNo);
+            cuListViewItem.SubItems.Add(customer.name);
+
+            cuListViewItem.SubItems.Add(customer.complain);
+
+            queueListView.Items.Add(cuListViewItem);
+
+ 
+
+            enqueueNameTextBox.Text="";
+            enqueueComplainTextBox.Text = "";
+        }
+
+       private void dequeueButton_Click(object sender, EventArgs e)
+      {
+           CustomerInfo customer=new CustomerInfo();
+           customer = customerInfos.Dequeue();
+           serialNoTextBox.Text = customer.serialNo;
+           dequeueNameTextBox.Text = customer.name;
+           dequeueComplainTextBox.Text = customer.complain;
+           queueListView.Items.RemoveAt(0);
+
+      }
+
+    }
+}
